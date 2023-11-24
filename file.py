b1c8589387f84c6e9d2d73173af8ba4d09372472
Navik <76463960+Barat-Tojiyev@users.noreply.github.com>
2023-11-24 10:49:57 +0500

    Update file.py telefon

--- file.py
+++ file.py
@@ -2,3 +2,5 @@ print('Hello World')
 son=input('Yoshingizni ayting:')
 son2=input('Tel raqamangizni ayting:+998')
 print(f'Sening yoshing {son}da, tel raqamingiz:+998{son2}')
+text="koʻramiz"
+print=(text)
